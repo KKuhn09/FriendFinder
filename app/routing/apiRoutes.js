e1e4d81901a45950a8ca388e788c30b7fdf22516
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-13 19:43:59 -0700

    Added comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,41 +9,43 @@ module.exports = function(app){
 		res.json(friends);
 	});
 
-	//API POST requests
+	//API POST requests that will match our user with a friend
 	app.post("/api/friends", function(req, res){
-		//Matching our user with a friend
+
+		//Object that will hold our "best match"
 		var bestMatch = {
 			name: "",
 			photo: "",
 			friendDifference: Infinity
 		};
-
+		//Parse the user survery
 		var userData = req.body;
 		var userScores = userData.scores;
-
 		var totalDifference;
 
+		//For each friend in our API
 		for(var i =0; i<friends.length; i++){
 			var currentFriend = friends[i];
 			totalDifference = 0;
-
 			console.log(currentFriend.name);
 
+			//For each score
 			for(var j=0; j<currentFriend.scores.length;j++){
 				var currentFriendScore = currentFriend.scores[j];
 				var currentUserScore = userScores[j];
-
+				//Calculate difference in scores and sum them into totalDifference
 				totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
 			}
-
+			//If totalDifference is less than the differences of the best match, replace bestMatch
 			if(totalDifference <= bestMatch.friendDifference){
 				bestMatch.name = currentFriend.name;
 				bestMatch.photo = currentFriend.photo;
 				bestMatch.friendDifference = totalDifference;
 			}
 		}
+		//Push user data to the database (API)
 		friends.push(userData);
-
+		//Return a JSON with the user's best match.
 		res.json(bestMatch);
 	});
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,11 @@
 var path = require("path");
 
 module.exports = function(app){
-
+	//Serve "/../public/survey.html" files when the route is /survey
 	app.get("/survey", function(req, res){
 		res.sendFile(path.join(__dirname, "/../public/survey.html"));
 	});
-
+	//If no matching route is found, default to home
 	app.get("*", function(req, res) {
 	  res.sendFile(path.join(__dirname, "/../public/home.html"));
 	});
